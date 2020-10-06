aa944e73cc811fbf8d4aef7230b9cea6170a0579
jorge <jordims2000@gmail.com>
2020-10-06 23:56:11 +0200

    nose

--- src/main/java/ehu/isad/ComboBoxExperiments.java
+++ src/main/java/ehu/isad/ComboBoxExperiments.java
@@ -1,34 +1,67 @@
 package ehu.isad;
 
+import com.google.gson.Gson;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class ComboBoxExperiments extends Application  {
 
+    private String coinmarket(String txanpon) {
+        String inputLine = "";
+        URL url;
+
+        try {
+            url = new URL("https://api.gdax.com/products/" + txanpon + "-eur/ticker");
+            URLConnection yc = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            inputLine = br.readLine();
+            br.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inputLine;
+    }
+
+    private float prezioaLortu(String mota) {
+        String emaitza = this.coinmarket(mota);
+        Gson gson = new Gson();
+        Txanpona txanpon = gson.fromJson(emaitza, Txanpona.class);
+        return txanpon.price;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("ComboBox Experiment 1");
+        primaryStage.setTitle("Txanponak");
 
         ComboBox comboBox = new ComboBox();
 
-        comboBox.getItems().add("Choice 1");
-        comboBox.getItems().add("Choice 2");
-        comboBox.getItems().add("Choice 3");
-
+        comboBox.getItems().add("BTC");
+        comboBox.getItems().add("ETH");
+        comboBox.getItems().add("LTC");
         comboBox.setEditable(true);
 
-        comboBox.setOnAction(e -> {
-            System.out.println( comboBox.getValue());
-        });
+        Label label = new Label(comboBox.getValue() + "=" + this.prezioaLortu((String) comboBox.getValue()) + "EUR");
 
-        HBox hbox = new HBox(comboBox);
+        comboBox.setOnAction(event -> label.setText("1 " + comboBox.getValue() + "=" + this.prezioaLortu((String) comboBox.getValue()) + "€"));
 
-        Scene scene = new Scene(hbox, 200, 120);
+        VBox vbox = new VBox(label, comboBox);
+
+        Scene scene = new Scene(vbox, 200, 120);
         primaryStage.setScene(scene);
         primaryStage.show();
 
